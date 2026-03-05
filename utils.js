b8e1f5dd45d0fc60ac2a1e93886e488fca64da4e
ismailgenctr-prog <ismailgenctr@gmail.com>
2026-03-05 21:45:05 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:41:02.534Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T18:45:07.846Z
+Commit: Add multi-language support
