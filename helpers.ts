2c2a5321b5a1fcd69f0e63b255e5300680a56c89
ismailgenctr-prog <ismailgenctr@gmail.com>
2026-03-07 20:29:09 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T17:29:07.214Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-07T17:29:09.383Z
+Commit: Fix broken links in documentation
